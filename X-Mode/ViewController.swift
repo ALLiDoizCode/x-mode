6f8b91e8244f38eed98c367c3861baba9438b6af
Jonathan Green <jgreen@jgreen01scl.corp.homestore.net>
2018-07-16 20:27:42 -0700

    Initial Commit

---
+++ X-Mode/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  X-Mode
+//
+//  Created by Green, Jonathan on 7/16/18.
+//  Copyright © 2018 Green, Jonathan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
